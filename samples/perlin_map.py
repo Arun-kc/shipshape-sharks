c12a1ae77be52149ea8ceb98e6711ba4dd36e13b
Raghav Joshi <raghavjoshi789@gmail.com>
2021-07-13 23:20:27 +0530

    Implemented better algorithm for map generation

--- samples/perlin_map.py
+++ samples/perlin_map.py
@@ -1,16 +1,23 @@
 import random
+from typing import Callable
 
-from perlin_noise import PerlinNoise
 from blessed import Terminal
+from perlin_noise import PerlinNoise
 
-rand_seed = int(10**5*random.random())
+
+def closest(ns: float, values: dict) -> Callable:
+    """Returns best match value form values"""
+    return values[min(values, key=lambda x: abs(ns*100 - x))]
+
+
+rand_seed = int(10 ** 5 * random.random())
 term = Terminal()
 noise = PerlinNoise(octaves=10, seed=rand_seed)
 
 green = {
-    0:term.on_chartreuse,
-    -6:term.on_chartreuse2,
-    -12:term.on_chartreuse3,
+    0: term.on_chartreuse,
+    -6: term.on_chartreuse2,
+    -12: term.on_chartreuse3,
 }
 
 trees = {
@@ -21,22 +28,24 @@ trees = {
 
 grass = {
     0: ' ',
-    # -5: '▓' 
+    # -5: '▓'
 }
 
 
 print(term.home+term.clear, end='')
 w, h = term.width, term.height
 
-pic = [[noise([i/w , j/h]) for j in range(h)] for i in range(w)]
+pic = [[noise([i/w, j/h]) for j in range(h)] for i in range(w)]
 s = ''
 for j in range(h):
     for i in range(w):
         ns = pic[i][j]
-        closest = lambda y:min(y, key=lambda x: abs(ns*100 - x))
         if ns > -0.25:
             ch = green[closest(green)](term.chartreuse3(grass[closest(grass)]))
         else:
             ch = term.steelblue4_on_steelblue2(random.choice(' '))
-        print(ch, end='')
-    print()
+        s += ch
+    s += '\n'
+
+if __name__ == '__main__':
+    print(s)
