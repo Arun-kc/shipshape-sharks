53b919d45b74920aaedd65e2a8fd0320bd571ee3
Parag <paragshendye@gmail.com>
2021-07-13 19:09:25 +0200

    fix in sizeof rectangle

--- samples/components/rectangle.py
+++ samples/components/rectangle.py
@@ -1,6 +1,3 @@
-from typing import overload
-
-
 class Rectangle:
     def __init__(self, x, y, width, height) -> None:
         self.x = x
@@ -8,8 +5,8 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    def __sizeof__(self) -> int:
-        return tuple(self.width, self.height)
+    def __sizeof__(self) -> tuple:
+        return self.width, self.height
 
     def isOverlapping(self, Other) -> bool:
         if(self.width == 0 or self.height == 0 or Other.width == 0 or Other.height == 0):
@@ -17,5 +14,6 @@ class Rectangle:
 
         return not ((self.x > (Other.x+Other.width)) or ((self.x+self.width) < Other.x) or (self.y > (Other.y+Other.height)) or ((self.y+self.height) < Other.height))
 
-    def isContaining(self, Other):
+    def isContaining(self, Other) -> bool:
         return Other.x >= self.x and (Other.x+Other.width) <= (self.x+self.width) and Other.y >= self.y and (Other.y+Other.height) <= (self.y+self.height)
+
